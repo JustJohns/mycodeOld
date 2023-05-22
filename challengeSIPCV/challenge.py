90800373729b0d3213868dffada19f618cce85ae
JustJohns <rcj148@gmail.com>
2023-05-22 22:23:06 +0000

    challenge

---
+++ challengeSIPCV/challenge.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#input user's name
+userName = input("What is your name?")
+
+#input day of the week
+dayOfWeek = input("What day of the week is it?")
+
+#print statement should read "Hello, <name>! Happy <day of the week>!
+print(f"Hello, {userName}! Happy {dayOfWeek}")
+
